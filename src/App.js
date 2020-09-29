caed412b9c1e7f01b67061594f37a3a9f747dcc2
thatzenbuhler <t.hatzenbuhler@gmail.com>
2020-09-29 11:07:10 -0700

    Initial setup + top bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import TopBar from './TopBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <TopBar></TopBar>
+      <p>
+        For the state in functional components, it is a little bit different.
+        Before the end of 2018, developers were not able to access state in functional
+        components at all. Functional components were therefore just used for returning
+        JSX logic. However, with the introduction of React Hooks, this has changed.
+        Now we can set and access state in functional components, and for newer code,
+        they are often preferred over class components. You will be exposed to both kinds
+        of components on the job, requiring us to be very familiar with both. The
+        way React hooks work is the topic of one of the following lectures, so don’t
+        worry about it too much right now. We are setting you up to say “Hey! I remember
+        that” when it’s introduced.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
---
+++ src/TopBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TopBar.css';
+
+const TopBar = () => {
+    return (
+        <h1 className="TopBar">
+            Tyler's React Todo List
+        </h1>
+    );
+};
+
+export default TopBar;
