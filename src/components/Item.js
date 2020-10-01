8f63216f5c1b1d1aead2658bf6b1aad4c2af25fe
thatzenbuhler <t.hatzenbuhler@gmail.com>
2020-10-01 11:17:13 -0700

    Added buttons for removing items

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,12 +2,21 @@ import React from 'react';
 import './Item.css';
 
 class Item extends React.Component {
+    constructor(props) {
+        super();
+    }
+
+    remove = () => {
+        console.log("remove item");
+    }
+
     render() {
         return (
             <div className="Item">
-                <p>
-                    TODO: {this.props.todo}
+                <p className="todo">
+                    <strong>TODO:</strong> {this.props.todo}
                 </p>
+                <button className="todo_button" onClick={this.remove}><strong>X</strong></button>
             </div>
         );
     }
