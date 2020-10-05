2dd4549ab8e3d2b57d0744bcc6ae05a61ee20e66
thatzenbuhler <t.hatzenbuhler@gmail.com>
2020-10-05 13:22:45 -0700

    Add a test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Check for top bar", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Tyler's React Todo List/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("Display's Example todo", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Example todo 1/i);
   expect(linkElement).toBeInTheDocument();
