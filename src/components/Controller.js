4224a1d974c82417a886044aefe1b6bcac55350f
thatzenbuhler <t.hatzenbuhler@gmail.com>
2020-10-06 10:18:59 -0700

    Improved input field usage

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -10,17 +10,30 @@ class Controller extends React.Component {
   }
 
   handleChange = (e) => {
+    this.setState({ textField: e.target.value });
     this.props.handleChange(e.target.value);
   };
 
   handleClick = () => {
     this.props.click(this.state.textField);
+    this.setState({ textField: "" });
   };
+
+  checkEnter = (e) => {
+    if (e.key === "Enter") {
+      this.handleClick();
+    }
+  }
 
   render() {
     return (
       <div className="Controller">
-        <input className="input" onChange={this.handleChange}></input>
+        <input
+          className="input"
+          value={this.state.textField}
+          onChange={this.handleChange}
+          onKeyPress={this.checkEnter}
+        ></input>
         <button className="button" onClick={this.handleClick}>
           Add to List
         </button>
